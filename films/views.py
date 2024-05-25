c38da2112e71ab2360afa6f3a2067a84c181131e
MrSWFL <mlrwhitehall@gmail.com>
2024-05-25 13:19:19 -0400

    Finished Video #1

--- films/views.py
+++ films/views.py
@@ -1,3 +1,4 @@
+# films/views.py
 from django.http.response import HttpResponse, HttpResponsePermanentRedirect
 from django.shortcuts import render
 from django.contrib.auth.views import LoginView
@@ -22,3 +23,28 @@ class RegisterView(FormView):
     def form_valid(self, form):
         form.save()  # save the user
         return super().form_valid(form)
+    
+def check_username(request):
+    username = request.POST.get("username")
+    if get_user_model().objects.filter(username=username).exists():
+        msg = """
+            <div role="alert" class="alert alert-error">
+                <svg xmlns="http://www.w3.org/2000/svg" class="stroke-current shrink-0 h-6 w-6" fill="none" viewBox="0 0 24 24"><path stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M10 14l2-2m0 0l2-2m-2 2l-2-2m2 2l2 2m7-2a9 9 0 11-18 0 9 9 0 0118 0z" /></svg>
+                <span>This username already exists!</span>
+            </div>
+        """
+        msg = """
+            <div style="color : red;"><h1>This username already exists</h1></div>
+        """
+        return HttpResponse(msg)
+    else:
+        msg = """
+            <div role="alert" class="alert alert-success">
+                <svg xmlns="http://www.w3.org/2000/svg" class="stroke-current shrink-0 h-6 w-6" fill="none" viewBox="0 0 24 24"><path stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M9 12l2 2 4-4m6 2a9 9 0 11-18 0 9 9 0 0118 0z" /></svg>
+                <span>This username is available</span>
+            </div>
+        """
+        msg = """
+            <div style="color : green;"><h1>This username is available</h1></div>
+        """
+        return HttpResponse(msg)
